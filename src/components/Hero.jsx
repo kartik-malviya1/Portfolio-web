e817b135fca6da712a19fad3f3c5d7c3f3546703
kartik-malviya1 <kartikmalviya30@gmail.com>
2024-06-27 01:01:44 +0530

    responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,25 +47,25 @@ const Hero = () => {
     <div className="w-full border-b border-neutral-900 p-2">
       <div className="flex flex-wrap sm:items-center">
         <div className="w-full lg:w-1/2">
-         <div className="flex flex-col ml-12 md:mt-10 mb-28 gap-7 items-center sm:items-start ">
+         <div className="flex flex-col ml-12 md:mt-10 mb-28 sm:gap-7 gap-4 items-center sm:items-start ">
             <motion.h1
             variants={container(0)}
             initial="hidden"
             animate="visible"
-            className=" text-6xl sm:text-7xl tracking-tighter lg:mt-16 mt-10
-            lg:text-8xl">Kartik Malviya</motion.h1>
+            className=" text-5xl sm:text-6xl tracking-tighter lg:mt-16 mt-10
+          ">Kartik Malviya</motion.h1>
             <motion.span
              variants={container(0.5)}
              initial="hidden"
              animate="visible"
-             className="bg-gradient-to-r from-pink-300 via-slate-400 to-purple-500 bg-clip-text text-3xl sm:text-4xl tracking-tight text-transparent sm:items-start mt-5">
+             className="bg-gradient-to-r from-pink-300 via-slate-400 to-purple-500 bg-clip-text text-2xl sm:text-4xl tracking-tight text-transparent sm:items-start mt-5">
               Full Stack Developer
             </motion.span>
             <motion.p
             variants={container(1)}
             initial="hidden"
             animate="visible"
-            className='my-2 max-w-xl py-6 sm:text-start text-start text-sm sm:text-base font-light tracking-tight'>{HERO_CONTENT}</motion.p>
+            className='my-2 max-w-xl py-6 pl-3 sm:text-start text-start text-sm sm:text-base font-light tracking-tight'>{HERO_CONTENT}</motion.p>
           </div>
         </div>
         <motion.div 
